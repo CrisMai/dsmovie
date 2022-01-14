94748cbbccd4a898598d7eb0e64471fa7da6251c
Cris Mai <crismai.dev@gmail.com>
2022-01-14 10:40:08 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,17 +1,27 @@
 
+import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { BASE_URL } from "utils/request";
 
 function Listing() {
+
+    //Forma errada
+
+    axios.get(`${BASE_URL}/movies?size=12&page=0`)
+        .then(response => {
+            console.log(response.data);
+
+        });
 
     return (
         <>
 
             <Pagination />
 
-            <div className="container"> 
+            <div className="container">
                 <div className="row">{/*usado breakpoints e grid system do Bootstrap */}
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3"> 
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                         <MovieCard />
                     </div>
                     <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
